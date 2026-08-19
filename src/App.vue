6af745b1745674fa00fcfe9fdde2aaa19488dab0
Felipe Fama <lipehfama.dev@gmail.com>
2026-08-19 12:09:41 -0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
+<script setup lang="ts">
+import Header from "./components/Header/Header.vue";
+import Sidebar from "./components/Sidebar/Sidebar.vue";
+import Footer from "./components/Footer/Footer.vue";
+</script>
+
 <template>
   <Header />
   <Sidebar />
   <router-view />
   <Footer />
 </template>
-
-<script setup lang="ts">
-import Header from "./components/Header/Header.vue";
-import Sidebar from "./components/Sidebar/Sidebar.vue";
-import Footer from "./components/Footer/Footer.vue";
-</script>
