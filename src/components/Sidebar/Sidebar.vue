841efe9587af3e6a6464c4374da8c939eefb4037
Felipe Fama <lipehfama.dev@gmail.com>
2026-08-19 13:38:55 -0300

    fix: improving the semantics of the sidebar component

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -17,11 +17,11 @@ const categories = computed(() => categoryStore.categories);
 </script>
 
 <template>
-  <aside :class="['sidebar', isSidebarOn ? 'hide-sidebar' : '']">
+  <menu :class="['sidebar', isSidebarOn ? 'hide-sidebar' : '']">
     <button aria-label="sidebar" type="button" class="sidebar-hide-btn" @click="setSidebarOff">
       <i class="bi bi-x bold-icon"></i>
     </button>
-    <div class="sidebar-cnt">
+    <section class="sidebar-cnt">
       <div class="cat-title fs-4 text-uppercase fw-bold ls-1h">All Categories</div>
       <ul class="cat-list">
         <li v-for="(category, idx) in categories" :key="idx" @click="setSidebarOff">
@@ -30,8 +30,8 @@ const categories = computed(() => categoryStore.categories);
           </router-link>
         </li>
       </ul>
-    </div>
-  </aside>
+    </section>
+  </menu>
 </template>
 
 <style scoped lang="scss">
